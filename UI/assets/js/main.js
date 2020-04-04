041c4b4763a756e0f9be2b06c46bb80009d90ef2
Sullivan Wisdom <wiztemple7@gmail.com>
2020-04-04 16:45:42 +0100

    dashboard style addition

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,42 +1,89 @@
 const actionsBtns = document.querySelectorAll(".app-sidebar-item-actions");
 const actionsDropDowns = document.querySelectorAll(".actions-dropdown");
 
-[...actionsBtns].map(actionBtn => {
+[...actionsBtns].map((actionBtn) => {
   actionBtn.addEventListener("click", () => {
-    [...actionsDropDowns].map(actionsDropDown => {
+    [...actionsDropDowns].map((actionsDropDown) => {
       actionsDropDown.classList.toggle("active");
     });
   });
 });
 
-// modal
 const modals = document.querySelectorAll(".modal");
 const buttons = document.querySelectorAll(".buttonJS");
 const cancelButtons = document.querySelectorAll(".close-modal");
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = [...modals].find(
-      modal => modal.dataset.id === button.dataset.target,
+      (modal) => modal.dataset.id === button.dataset.target,
     );
     modal.style.display = "block";
   });
 });
 
-cancelButtons.forEach(cancelButton => {
+cancelButtons.forEach((cancelButton) => {
   cancelButton.addEventListener("click", () => {
-    modals.forEach(modal => {
+    modals.forEach((modal) => {
       modal.style.display = "none";
     });
   });
 });
-// dismiss modal when the window is clicked
-document.addEventListener("click", e => {
-  modals.forEach(modal => {
+document.addEventListener("click", (e) => {
+  modals.forEach((modal) => {
     if (modal.style.display === "block") {
       if (e.target.className === "modal") {
         modal.style.display = "none";
       }
     }
   });
+});
+
+// /* Javascript only */
+// function displayDropdown(element) {
+//   const dropdowns = document.getElementsByClassName("actions-dropdown");
+//   let i;
+//   for (i = 0; i < dropdowns.length; i++) {
+//     dropdowns[i].classList.remove("active");
+//   }
+//   // element.nextSibling is the carriage return… need to go to the next next to point on the dropdown menu
+//   element.nextSibling.nextSibling.classList.toggle("active");
+// }
+
+/* W3C function to close the dropdown when clicked outside. */
+// window.onclick = function(event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     const dropdowns = document.getElementsByClassName("dropdown-content");
+//     let i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       const openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
+// const arr = [1, 4, 2, 6, 7, 0];
+
+// const a = (str) => str + "!";
+
+// console.log(a("wiz"));
+
+// arr.map((c) => console.log(c));
+
+$(".js-dropdown-toggler").click(function (e) {
+  var current = $(this).next(".js-dropdown");
+  current.toggleClass("is-open");
+  $(".js-dropdown").not(current).removeClass("is-open");
+  e.stopPropagation();
+});
+
+$("html").click(function () {
+  if ($(".js-dropdown").hasClass("is-open")) {
+    $(".js-dropdown").removeClass("is-open");
+  }
+});
+
+$(".js-dropdown").click(function (e) {
+  e.stopPropagation();
 });
